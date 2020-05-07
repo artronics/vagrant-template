4b27babf5c3cd20ba5505650db58d6b429081b28
jhosseini <jalalhosseiny@gmail.com>
2020-05-07 19:51:39 +0100

    install neovim

--- scripts/vagrant_provision.sh
+++ scripts/vagrant_provision.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
+echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt/sources.list
 apt-get update
 apt-get upgrade
-apt-get install -y zsh curl wget git fuse python zip make cmake build-essential "linux-headers-$(uname -r)"
+apt-get install -y zsh curl wget git ctags fd-find fzf fuse python zip make cmake build-essential "linux-headers-$(uname -r)"
 
 chsh --shell /bin/zsh vagrant
 cd /tmp
@@ -18,6 +19,9 @@ sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="avit"/g' /home/vagrant/.zshrc
 
 touch .aliases
 
+echo "Install Rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
 echo "Setup vimrc for neovim"
 wget -q -P .config/nvim/ https://gist.githubusercontent.com/artronics/171b1d00342687814a3effd31138b790/raw/0fb3f34049d1cef97ef101cefdaaf271acb98393/vimrc
 mv .config/nvim/vimrc .config/nvim/init.vim
@@ -26,6 +30,8 @@ echo "Install vim plugins"
 curl -sfLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim --headless +PlugInstall +qall > /dev/null
 
+echo "Update PATH"
+echo "export PATH=$PATH:$HOME/.cargo/bin" >> /home/vagrant/.zshrc
 echo "Done!"
 END
 )
